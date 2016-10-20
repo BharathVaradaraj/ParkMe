a1dc4e7833489f12ad0fe75eadb264628c722868
BharathVaradaraj <bharathvaradaraj96@gmail.com>
2016-10-20 21:49:33 +0530

    Update Update.py
    This is used to read the output of the sensors through arduino uno and update the status in the database.

--- Update.py
+++ Update.py
@@ -1,1 +1,24 @@
+import serial
+from firebase import firebase
+firebase = firebase.FirebaseApplication("Your database url")
+
+def update_status(id,status):
+    x = firebase.get("sensor","")
+    status = str(status)
+    for i in x:
+        print(x[i]["SensorID"])
+        if x[i]["SensorID"] == id:
+            firebase.put('/sensor/' + i ,"status" ,status )
+
+
+s = serial.Serial("COM5", 9600)
+
+while True:
+    x = s.readline()
+    sensor_id = str(x)[10:13]
+    status = str(x)[14]
+    print("sensor_id =",sensor_id)
+    print(status)
+    update_status(sensor_id,status)
+
 
